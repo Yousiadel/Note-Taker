a05f41a8bce8c362466345e9602990d808326a9f
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-07-21 22:09:18 -0400

    NoteTaker

--- db/store.js
+++ db/store.js
@@ -5,9 +5,6 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-    constructor() {
-        this.lastId = 0;
-    }
 
     read() {
         return readFileAsync("db/db.json", "utf8");
